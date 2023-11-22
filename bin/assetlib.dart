3de752c501163ad3beddf768ab7eda6eec736456
Zerebel <86872654+Zerebel@users.noreply.github.com>
2023-11-22 03:09:24 -0600

    Add initial project files

---
+++ bin/assetlib.dart
@@ -0,0 +1,122 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:assetlib/main.dart';
+
+void main(List<String> args) {
+  exitCode = 0; // presume success
+
+  Generator(args);
+}
+
+class Generator with AssetClass {
+  /// The command-line arguments.
+  final List<String> _args;
+  ArgResults? results;
+
+  /// Create an instance of [Generator] with the given command-line arguments.
+  /// - [args] - The command-line arguments.
+  Generator(this._args) {
+    results = arguments.parse(_args);
+
+    if (results!['version']) {
+      stdout.writeln('assetlib version: $version');
+      exit(1);
+    }
+
+    if (results!['help']) {
+      stdout.writeln('A command-line tool to generate an asset class.\n');
+      stdout.writeln('Usage: dart assetlib [options]\n');
+      stdout.writeln('Global options:');
+      stdout.write('${arguments.usage}\n');
+      stdout.writeln('Available commands:');
+      exit(1);
+    }
+
+    init();
+  }
+
+  /// Initialize the generator.
+  /// - [results] - The command-line arguments.
+  init() {
+    final className = results?['class'] ?? defaultClassName;
+
+    final filePath = results?['output'] ?? defaultOutput;
+
+    if (FileSystemEntity.isDirectorySync(filePath)) {
+      stdout.writeln('Generating $className in $filePath...');
+    } else {
+      stdout.writeln('The output path is not a directory.');
+      stdout.writeln('Run `assetlib --help` for more information.');
+      exit(1);
+    }
+
+    final File classFile = File(filePath + '/$defaultFileName');
+
+    final IOSink sink = classFile.openWrite();
+
+    for (var i = 0; i < 3; i++) {
+      sink.writeln("///* GENERATED CODE - DO NOT MODIFY BY HAND *///");
+    }
+
+    sink.writeln('');
+    sink.writeln('class $className {');
+    sink.writeln('  $className._();\n');
+
+    for (var path in defaultPaths) {
+      sink.writeln('/// $path');
+      if (FileSystemEntity.isDirectorySync(path)) {
+        _writeAssetsFromDirectory(Directory(path), sink);
+      } else {
+        _writeAssetFromFile(File(path), sink);
+      }
+      sink.writeln('');
+    }
+
+    sink.writeln('}');
+    sink.close();
+  }
+
+  final List<String> _writtenAssets = [];
+
+  _writeAssetsFromDirectory(Directory directory, IOSink sink) {
+    final List<FileSystemEntity> entities =
+        directory.listSync().skipWhile((entity) {
+      return FileSystemEntity.isDirectorySync(entity.path);
+    }).toList();
+
+    if (entities.isEmpty) {
+      stdout.writeln('No assets found in ${directory.path}.....');
+      return;
+    }
+
+    stdout.writeln(
+        'Found ${entities.length} file${entities.length > 1 ? 's' : ''} in ${directory.path}');
+
+    for (var entity in entities) {
+      stdout.writeln('');
+
+      _writeAssetFromFile(entity, sink);
+    }
+  }
+
+  _writeAssetFromFile(FileSystemEntity entity, IOSink sink) {
+    final String name = entity.path.split('/').last;
+    final String key = name.split('.').first;
+    final String value = entity.path;
+
+    final generatedAsset = '  static const String $key = \'$value\';';
+
+    if (_writtenAssets.contains(generatedAsset)) {
+      stdout.writeln('Asset $name already exists.');
+      return;
+    }
+
+    sink.writeln(generatedAsset);
+    _writtenAssets.add(generatedAsset);
+  }
+
+  factory Generator.fromArgs(List<String> args) {
+    return Generator(args);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:yaml/yaml.dart';
+
+mixin AssetClass {
+  /// The [ArgParser] for parsing the command-line arguments.
+  /// - [path] - Path to the asset directory.
+  /// - [class] - Name of the generated asset class.
+  /// - [output] - Path to the generated asset class file.
+  /// - [prefix] - Prefix to add to the asset class members.
+  /// - [help] - Show this help message.
+  /// - [version] - Show the version of this application.
+  ArgParser get arguments => ArgParser()
+    ..addOption('path',
+        abbr: 'p', defaultsTo: '.', help: 'Path to the asset directory.')
+    ..addOption('class',
+        abbr: 'c',
+        defaultsTo: defaultClassName,
+        help: 'Name of the generated asset class.')
+    ..addOption('output',
+        abbr: 'o',
+        defaultsTo: defaultOutput,
+        help: 'Path to the generated asset class file.')
+    ..addOption('prefix',
+        abbr: 'x',
+        defaultsTo: '',
+        help: 'Prefix to add to the asset class members.')
+    ..addFlag('help',
+        abbr: 'h', negatable: false, help: 'Show this help message.')
+    ..addFlag('version',
+        abbr: 'v',
+        negatable: false,
+        help: 'Show the version of this application.');
+
+  /// The content of the [pubspec.yaml] file.
+  final _pubspec = loadYaml(File('pubspec.yaml').readAsStringSync());
+
+  /// - [version] - Version of the application.
+  String get version => _pubspec['version'];
+
+  /// Default Directory for assets from pubspec.yaml
+  /// - [path] - Default Path to the asset directory.
+  ///     default: 'assets/'
+  YamlList get defaultPaths {
+    if (_pubspec['flutter'] == null) {
+      throw Exception('flutter section not found in pubspec.yaml');
+    }
+    return _pubspec['flutter']['assets'];
+  }
+
+  /// - [Class] - Default Name of the generated asset class.
+  ///    default: 'ProjectNameAssets'
+  String get defaultClassName {
+    if (_pubspec['name'] == null) {
+      throw Exception('name section not found in pubspec.yaml');
+    }
+
+    // capitalize first word
+    final name = _pubspec['name'].toString().split(' ').map((word) {
+      return word[0].toUpperCase() + word.substring(1);
+    }).join('');
+
+    return '${name}Assets';
+  }
+
+  /// - [File] - Default name of the generated asset class file.
+  ///
+  String get defaultFileName {
+    if (_pubspec['name'] == null) {
+      throw Exception('name section not found in pubspec.yaml');
+    }
+
+    final name = _pubspec['name'] as String;
+
+    return '${name}_assets.dart';
+  }
+
+  /// - [output] - Default Path to the generated asset class file.
+  ///   default: 'lib/assets.dart'
+  String get defaultOutput {
+    final String path = 'lib/';
+    if (!(Directory(path).existsSync())) {
+      throw Exception('lib directory not found');
+    }
+
+    return path;
+  }
+}
