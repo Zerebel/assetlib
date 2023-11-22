d99a6bf74d46a7a77a39bc015a11934e244fb9cb
Zerebel <86872654+Zerebel@users.noreply.github.com>
2023-11-22 03:09:35 -0600

    Add new assets: pubg.svg, menu.svg, and video.mp4

---
+++ lib/assetlib_assets.dart
@@ -0,0 +1,17 @@
+///* GENERATED CODE - DO NOT MODIFY BY HAND *///
+///* GENERATED CODE - DO NOT MODIFY BY HAND *///
+///* GENERATED CODE - DO NOT MODIFY BY HAND *///
+
+class AssetlibAssets {
+  AssetlibAssets._();
+
+/// assets/menu.svg
+  static const String menu = 'assets/menu.svg';
+
+/// assets/images/
+  static const String pubg = 'assets/images/pubg.svg';
+
+/// assets/
+  static const String video = 'assets/video.mp4';
+
+}
