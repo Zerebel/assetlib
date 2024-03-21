7122516b18f8a57170c65766de64b094d584dad1
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-03-21 02:58:07 +0000

    example output file

--- lib/assetlib_assets.dart
+++ lib/assetlib_assets.dart
@@ -8,9 +8,21 @@ class AssetlibAssets {
   /// assets/menu.svg
   static const String menu = 'assets/menu.svg';
 
-  /// assets/images/
+  /// assets/images/icon.svg
+  static const String icon = 'assets/images/icon.svg';
+
+  /// assets/images/pubg.svg
   static const String pubg = 'assets/images/pubg.svg';
 
-  /// assets/
+  /// assets/video.mp4
   static const String video = 'assets/video.mp4';
+
+  /// assets/icons/icon.png
+  static const String icon1 = 'assets/icons/icon.png';
+
+  /// assets/icons/icon.svg
+  static const String icon2 = 'assets/icons/icon.svg';
+
+  /// assets/icons/content/heart.svg
+  static const String heart = 'assets/icons/content/heart.svg';
 }
